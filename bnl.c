f1a0e53f332e344697610ca0faf3ea92702b1d89
Shaun <shaun@mindsnare.co>
2019-10-02 18:23:28 +1000

    initial commit

---
+++ bnl.c
@@ -0,0 +1,5 @@
+
+#include "bnl.h"
+
+
+
---
+++ bnl.h
@@ -0,0 +1,14 @@
+
+
+
+
+typedef struct {
+    char *nstr;
+    int len;
+
+}bnl;
+
+bnl *bin_add(bnl *a, bnl *b);
+char *b_add(char *, char *, int len);
+
+bnl *init(int len);
